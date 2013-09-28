73405588e5e4f931150812ac0c1046b1814716b3
christopher james churchill <churchill.c.j@gmail.com>
2013-09-28 17:11:39 +0200

    Update Ajax_googlemap.js

--- Ajax_googlemap.js
+++ Ajax_googlemap.js
@@ -1,10 +1,12 @@
 jQuery(document).ready(function ($) {
-	    /********************************************************smooth ajax after load************************************/
-    function scrollToAnchor(AnId){
-    var aTag = $("#"+ AnId);
-    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-}
-/*****************************INIT MAP**************************************************************/
+    /********************************************************smooth ajax after load************************************/
+    function scrollToAnchor(AnId) {
+        var aTag = $("#" + AnId);
+        $('html,body').animate({
+            scrollTop: aTag.offset().top
+        }, 'slow');
+    }
+    /*****************************INIT MAP**************************************************************/
     function initialize() {
         google.maps.visualRefresh = true;
         var myLatlng = new google.maps.LatLng(51.201465, -0.30244);
@@ -15,30 +17,30 @@ jQuery(document).ready(function ($) {
             scrollwheel: false,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-/*
-**Load Map onto dom  using tag with id="map-canvas"
-**
-*/
-        var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+        /*
+         **Load Map onto dom  using tag with id="map-canvas"
+         **
+         */
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-/*
-**
-**Load KML file with Rand variable to stop caching and make it easier to devolpe 
-**REMOVE  + (new Date()).valueOf() when pushing to production
-**
-*/
+        /*
+         **
+         **Load KML file with Rand variable to stop caching and make it easier to devolpe
+         **REMOVE  + (new Date()).valueOf() when pushing to production
+         **
+         */
         var kmlLayer = new google.maps.KmlLayer({
             url: 'http://cga-golf.com/new/wp-content/themes/required-starter/CGAGolfAcademies.kml?rand=' + (new Date()).valueOf(),
             suppressInfoWindows: true,
             map: map
         });
-/*
-**Set Info bubble content using  infobox: https://code.google.com/p/google-maps-utility-library-v3/wiki/Libraries
-*/
+        /*
+         **Set Info bubble content using  infobox: https://code.google.com/p/google-maps-utility-library-v3/wiki/Libraries
+         */
 
         function showInContentWindow(position, title, text) {
-            var content = "<div class='info_win'><h3>"+ title +"</h3><p>" + text + "</p></div>";
-   				 infowindow = new InfoBox({
+            var content = "<div class='info_win'><h3>" + title + "</h3><p>" + text + "</p></div>";
+            infowindow = new InfoBox({
                 content: content,
                 disableAutoPan: false,
                 maxWidth: 0,
@@ -57,46 +59,46 @@ jQuery(document).ready(function ($) {
                 pane: "floatPane",
                 enableEventPropagation: false
             });
-/*
-**look for dom ready and intialize ajax on each infobox
-** You need to change #content_two to your wrapper you want to load the content into
-** and add a tag with a class of back to reload the old content... 
-*/
-       google.maps.event.addListener(infowindow, 'domready', function() {
-			    /******AJAX MAP ****/
-			    siteURL = 'http://' + top.location.host.toString();
-			    coachesLinks = jQuery('.info_win a');
-			    coachesLinks.click(function (e) {
-			        e.preventDefault();
-			    });
-			    coachesLinks.click(function (e) {
-			        $el = jQuery(this);
-			        URL = $el.attr('href');
-			        shareurl = $el.attr('href');
-			        URL = URL + " .main";
-			        jQuery('#content_two').show('slow').load(URL, function () {
-			            scrollToAnchor('content_two');
-			            $('.main').css('overflow', 'visible');
-			            $('#content_two').css('overflow', 'visible');
-			            jQuery('#content_two .back').on('click', function () {
-			                jQuery(this).hide('slow');
-			                var contentTwo = jQuery('#content_two');
-			                if (contentTwo.is(':hidden')) {
-			                    jQuery('#content_two .back').hide();
-			                } else {
-			                    contentTwo.hide('slow');
-			                    jQuery('#content > .main').show('slow');
-			                    jQuery('#content > .main').css('overflow', 'visible');
-			                    scrollToAnchor('access');
-			                }
-			            });
+            /*
+             **look for dom ready and intialize ajax on each infobox
+             ** You need to change #content_two to your wrapper you want to load the content into
+             ** and add a tag with a class of back to reload the old content...
+             */
+            google.maps.event.addListener(infowindow, 'domready', function () {
+                /******AJAX MAP ****/
+                siteURL = 'http://' + top.location.host.toString();
+                coachesLinks = jQuery('.info_win a');
+                coachesLinks.click(function (e) {
+                    e.preventDefault();
+                });
+                coachesLinks.click(function (e) {
+                    $el = jQuery(this);
+                    URL = $el.attr('href');
+                    shareurl = $el.attr('href');
+                    URL = URL + " .main";
+                    jQuery('#content_two').show('slow').load(URL, function () {
+                        scrollToAnchor('content_two');
+                        $('.main').css('overflow', 'visible');
+                        $('#content_two').css('overflow', 'visible');
+                        jQuery('#content_two .back').on('click', function () {
+                            jQuery(this).hide('slow');
+                            var contentTwo = jQuery('#content_two');
+                            if (contentTwo.is(':hidden')) {
+                                jQuery('#content_two .back').hide();
+                            } else {
+                                contentTwo.hide('slow');
+                                jQuery('#content > .main').show('slow');
+                                jQuery('#content > .main').css('overflow', 'visible');
+                                scrollToAnchor('access');
+                            }
+                        });
 
-			        });
-			        $('#content > .main').hide('slow');
-			    });
+                    });
+                    $('#content > .main').hide('slow');
+                });
 
-		}); 
-infowindow.open(map); 
+            });
+            infowindow.open(map);
 
         }
         google.maps.event.addListener(kmlLayer, 'click', function (kmlEvent) {
