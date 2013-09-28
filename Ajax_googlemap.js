f72c704b89bb7c64342a7cad1fa1a10587bb4454
christopher james churchill <churchill.c.j@gmail.com>
2013-09-28 17:01:09 +0200

    Update Ajax_googlemap.js

--- Ajax_googlemap.js
+++ Ajax_googlemap.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function ($) {
     var aTag = $("#"+ AnId);
     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
 }
+/*****************************INIT MAP**************************************************************/
     function initialize() {
         google.maps.visualRefresh = true;
         var myLatlng = new google.maps.LatLng(51.201465, -0.30244);
@@ -14,14 +15,26 @@ jQuery(document).ready(function ($) {
             scrollwheel: false,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
+/*
+**Load Map onto dom  using tag with id="map-canvas"
+**
+*/
         var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
+/*
+**
+**Load KML file with Rand variable to stop caching and make it easier to devolpe 
+**REMOVE  + (new Date()).valueOf() when pushing to production
+**
+*/
         var kmlLayer = new google.maps.KmlLayer({
-            url: 'https://developers.google.com/kml/documentation/KML_Samples.kml?rand=' + (new Date()).valueOf(),
+            url: 'http://cga-golf.com/new/wp-content/themes/required-starter/CGAGolfAcademies.kml?rand=' + (new Date()).valueOf(),
             suppressInfoWindows: true,
             map: map
         });
-
+/*
+**Set Info bubble content using  infobox: https://code.google.com/p/google-maps-utility-library-v3/wiki/Libraries
+*/
 
         function showInContentWindow(position, title, text) {
             var content = "<div class='info_win'><h3>"+ title +"</h3><p>" + text + "</p></div>";
@@ -44,6 +57,11 @@ jQuery(document).ready(function ($) {
                 pane: "floatPane",
                 enableEventPropagation: false
             });
+/*
+**look for dom ready and intialize ajax on each infobox
+** You need to change #content_two to your wrapper you want to load the content into
+** and add a tag with a class of back to reload the old content... 
+*/
        google.maps.event.addListener(infowindow, 'domready', function() {
 			    /******AJAX MAP ****/
 			    siteURL = 'http://' + top.location.host.toString();
